f0e3a0bb56ec95f0b2bab8fe3d321ed2fa91b46e
ultr4 <cl.rch09@icloud.com>
2024-02-16 13:38:40 -0300

    :sparkles: feat: Create RequestsExceptionHandler

---
+++ src/main/java/com/example/crud/infra/RequestsExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.example.crud.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class RequestsExceptionHandler {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> threat404() {
+        var response = new ExceptionDTO("ID not found.");
+        return ResponseEntity.badRequest().body(response.message);
+    }
+}
