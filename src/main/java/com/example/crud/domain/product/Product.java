5b7e0482996003a138bfe03ec93a47f144ff7576
ultr4 <cl.rch09@icloud.com>
2024-02-16 13:39:18 -0300

    :lipstick: feat: Add an indent to lines

--- src/main/java/com/example/crud/domain/product/Product.java
+++ src/main/java/com/example/crud/domain/product/Product.java
@@ -1,15 +1,7 @@
 package com.example.crud.domain.product;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Table(name = "product")
 @Entity(name = "product")
@@ -19,19 +11,19 @@ import lombok.Setter;
 @NoArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Product {
-  @Id
-  @GeneratedValue(strategy = GenerationType.UUID)
-  private String id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
 
-  private String name;
+    private String name;
 
-  private Integer price_in_cents;
+    private Integer price_in_cents;
 
-  private Boolean active;
+    private Boolean active;
 
-  public Product(RequestProduct requestProduct) {
-    this.name = requestProduct.name();
-    this.price_in_cents = requestProduct.price_in_cents();
-    this.active = true;
-  }
+    public Product(RequestProduct requestProduct) {
+        this.name = requestProduct.name();
+        this.price_in_cents = requestProduct.price_in_cents();
+        this.active = (Boolean) true;
+    }
 }
